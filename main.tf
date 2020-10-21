268ca4eea21c93a0248b5b1486ab4af4298fbfbe
Gireesh439 <37291626+Gireesh439@users.noreply.github.com>
2020-10-21 16:36:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "vnet" {
     name                = "myTFVnet"
     address_space       = ["10.0.0.0/16"]
-    location            = "westus2"
-    resource_group_name = azurerm_resource_group.rg.name
+    location            = "EAST US"
+    resource_group_name = "user-aujefjgfzsfh"
 }
